01a962332ccff016e4cadfaf9e6e31203a481d76
aste-0973 <152900534+Astefora@users.noreply.github.com>
2024-02-15 16:27:28 -0500

    f
    new amazon

--- Amazone-clone-project/amazone-clone/src/App.js
+++ Amazone-clone-project/amazone-clone/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Header from "./Component/Header/Header";
+import Carousel from "./Component/Caruosel/Carousel";
+import Category from "./Component/Category/category";
+import Product from "./Component/Products/Product";
+import './index.css'
+import Routing from "./Router";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+      <Routing/>
+    </>
   );
 }
 
